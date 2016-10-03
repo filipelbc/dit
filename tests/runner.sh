0698026a7fade4c1845c846a2cb8c5242d49975a
Daniel Moraes <daniel.b.moraes@gmail.com>
2016-10-03 18:12:17 -0300

    Added extra test

--- tests/runner.sh
+++ tests/runner.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
-for i in test_*.sh; do
-    echo -ne "$i    "
-    out=${i%.*}.out
-    ok=${i%.*}.ok
-    diff=${i%.*}.diff
-    ./$i > $out 2>&1
-    diff $out $ok > $diff
-    if [ -s $diff ]; then
-        echo "fail"
-        exit 1
-    else
-        echo "pass"
-    fi
-done
+run_tests() {
+    for i in "$@"; do
+        echo -ne "$i    "
+        out=${i%.*}.out
+        ok=${i%.*}.ok
+        diff=${i%.*}.diff
+        ./$i > $out 2>&1
+        diff $out $ok > $diff
+        if [ -s $diff ]; then
+            echo "fail"
+            exit 1
+        else
+            echo "pass"
+        fi
+    done
+}
+
+run_tests $(ls test_*.sh)
