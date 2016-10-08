7d7da4c8eae395dbad72d942de882e8c31dffb20
filipelbc <filipelbc@gmail.com>
2016-10-08 08:52:11 -0300

    Change in tests makefile structure

--- tests/runner.sh
+++ tests/runner.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 
-run_tests() {
-    for i in "$@"; do
-        echo -ne "$i    "
-        out=${i%.*}.out
-        ok=${i%.*}.ok
-        diff=${i%.*}.diff
-        ./$i > $out 2>&1
-        diff $out $ok > $diff
-        if [ -s $diff ]; then
-            echo "fail"
-            exit 1
-        else
-            echo "pass"
-        fi
-    done
-}
-
-run_tests $(ls test_*.sh)
+for i in $@
+do
+    echo -ne "$i    "
+    out=${i%.*}.out
+    ok=${i%.*}.ok
+    diff=${i%.*}.diff
+    ./$i > $out 2>&1
+    diff $out $ok > $diff
+    if [ -s $diff ]; then
+        echo "fail"
+        exit 1
+    else
+        echo "pass"
+    fi
+done
