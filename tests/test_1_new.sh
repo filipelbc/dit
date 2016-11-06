5b17148f1f90ad3652b55fc22cb85692ba7015b0
filipelbc <filipelbc@gmail.com>
2016-11-06 11:11:13 -0200

    Update tests

--- tests/test_1_new.sh
+++ tests/test_1_new.sh
@@ -1,29 +1,20 @@
 #!/usr/bin/env bash
 
 ./ditcmd new t1 -: '. . t1'
-
 ./ditcmd new g1/t2 -: 'g1 . t2'
-
 ./ditcmd new g2/g3/t3 -: 'g2 g3 t3'
-
 ./ditcmd new /t4 -: '. . t4'
-
 ./ditcmd new /g4/t5 -: 'g4 . t5'
-
 ./ditcmd new g5//t6 -: 'g5 . t6'
-
 ./ditcmd new //t7 -: '. . t7'
-
 ./ditcmd new g5/g6/t8 -: 'g5 g6 t8'
-
 ./ditcmd new g5//t9 -: 'g5 . t9'
-
-./ditcmd new ././t7 -: '. . t7, fails: already exists'
-
 ./ditcmd new t10 -: '. . t10'
 
-./ditcmd new :abu -: 'fails: invalid task name'
-
-./ditcmd new .bo/bar -: 'fails: invalid group name'
+./ditcmd new ././t7 -: '. . t7, fails: already exists'
+./ditcmd new _foo -: 'fails: invalid task name'
+./ditcmd new .foo -: 'fails: invalid task name'
+./ditcmd new _foo/bar -: 'fails: invalid group name'
+./ditcmd new .foo/bar -: 'fails: invalid group name'
 
 ./list
